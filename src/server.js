15dc020e44feb87d1192f589f08d87e2ee54715a
gzlin7 <gloriazlin@yahoo.com>
2025-05-22 16:01:00 -0400

    Clients initial

--- src/server.js
+++ src/server.js
@@ -20,7 +20,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// Socket.IO signaling for WebRTC
+// Simple matchmaking system to pair two players into a "room"
+let waitingPlayer = null;
+let roomCounter = 1;
+
 io.on("connection", (socket) => {
   socket.on("signal", (data) => {
     // Relay signaling data to the target peer
@@ -34,6 +37,35 @@ io.on("connection", (socket) => {
     // Send the list of other clients to the new client
     const clients = Array.from(io.sockets.sockets.keys()).filter(id => id !== socket.id);
     socket.emit("peers", clients);
+  });
+
+  // Matchmaking: pair two players into a room
+  socket.on("join-game", () => {
+    if (waitingPlayer && waitingPlayer.connected) {
+      // Pair with waiting player
+      const room = "room" + roomCounter++;
+      socket.join(room);
+      waitingPlayer.join(room);
+
+      // Notify both players of their roles
+      socket.emit("game-start", { role: "opponent", room });
+      waitingPlayer.emit("game-start", { role: "you", room });
+
+      waitingPlayer = null;
+    } else {
+      // Wait for another player
+      waitingPlayer = socket;
+      socket.emit("waiting");
+    }
+  });
+
+  // Relay game actions to the other player in the room
+  socket.on("game-action", ({ room, action, data }) => {
+    socket.to(room).emit("game-action", { action, data });
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingPlayer === socket) waitingPlayer = null;
   });
 });
 
